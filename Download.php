a7e2c36b1776034a365bdd1c9467c18fde91f3f1
Ray <89938940+raytran54@users.noreply.github.com>
2024-05-08 10:37:32 +0700

    Add files via upload
    File cho func Download

---
+++ Download.php
@@ -0,0 +1,17 @@
+<?php
+$file = 'uploads/BRcpUxD9.txt'; // Đường dẫn tới file cần tải xuống
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+} else {
+    echo "File không tồn tại.";
+}
+?>
